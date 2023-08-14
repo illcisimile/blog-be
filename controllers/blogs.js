4e34932c634cba6156cb770a528bd488ce179b53
Jarod Bernabe <jarodbernabe1100@gmail.com>
2023-08-14 20:47:06 +0800

    added update

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -10,8 +10,11 @@ blogsRouter.get('/', async (request, response) => {
   response.status(200).json(blogs);
 });
 
-blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id);
+blogsRouter.get('/:blogId', async (request, response) => {
+  const blog = await Blog.findById(request.params.blogId).populate('author', {
+    name: 1,
+    username: 1,
+  });
 
   if (blog) {
     response.status(200).json(blog);
@@ -33,12 +36,53 @@ blogsRouter.post('/', userExtractor, async (request, response) => {
   });
 
   const savedBlog = await blog.save();
-  const populatedSavedBlog = await savedBlog.populate('author', { name: 1 });
+
+  const populatedSavedBlog = await savedBlog.populate('author', {
+    name: 1,
+    username: 1,
+  });
 
   user.blogs = user.blogs.concat(savedBlog.id);
   await user.save();
 
   response.status(201).json(populatedSavedBlog);
+});
+
+blogsRouter.put('/:blogId', userExtractor, async (request, response) => {
+  const body = request.body;
+  const user = request.user;
+
+  const blog = await Blog.findById(request.params.blogId);
+
+  if (!blog) {
+    return response.status(404).json({ error: 'blog does not exist' });
+  }
+
+  if (blog.author.toString() !== user.id) {
+    return response.status(401).json({ error: 'unauthorized' });
+  }
+
+  const updatedBlogObject = {
+    title: body.title,
+    description: body.description,
+    content: body.content,
+    tags: body.tags,
+    author: user.id,
+    updatedAt: Date.now(),
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.blogId,
+    updatedBlogObject,
+    { new: true }
+  );
+
+  const populatedUpdatedBlog = await updatedBlog.populate('author', {
+    name: 1,
+    username: 1,
+  });
+
+  response.status(200).json(populatedUpdatedBlog);
 });
 
 module.exports = blogsRouter;
